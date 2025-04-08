4090ff3383a3a32d5d180660f0145174f12b434a
HyeonJun <hyeonjun1968@naver.com>
2025-04-08 21:16:51 +0900

    chore: AI + VectorStore 환경 설정 완료
    - application.yml에 .env 파일 불러오기 성공
    - OpenAI API 키 설정
    - Pinecone API 키 설정

--- src/main/java/a4/streamx_be/StreamxBeApplication.java
+++ src/main/java/a4/streamx_be/StreamxBeApplication.java
@@ -1,11 +1,12 @@
 package a4.streamx_be;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class StreamxBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StreamxBeApplication.class, args);
     }
