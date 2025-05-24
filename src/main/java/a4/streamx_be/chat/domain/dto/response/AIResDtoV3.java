1a3d32c6d5b5b2b076a60fb82e05b9a27199320c
HyeonJun <hyeonjun1968@naver.com>
2025-05-24 12:55:35 +0900

    refactor : DTO 수정
    - AIReqDtoV1 : prompt 삭제 사용자 메세지만 받을 수 있도록 변경
    - AIResDtoV3 : Builder 어노테이션 추가

--- src/main/java/a4/streamx_be/chat/domain/dto/response/AIResDtoV3.java
+++ src/main/java/a4/streamx_be/chat/domain/dto/response/AIResDtoV3.java
@@ -2,7 +2,9 @@ package a4.streamx_be.chat.domain.dto.response;
 
 import a4.streamx_be.chat.domain.model.Emotion;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
+@Builder
 public record AIResDtoV3(
         @JsonProperty("aiText") String aiText,
         @JsonProperty("emotion") Emotion emotion,
