c70cb60b0d3f1555a864943df766fc7d454766dd
HyeonJun <hyeonjun1968@naver.com>
2025-06-03 23:39:10 +0900

    refactor : 사용자 정보 API 내용 수정
    - 사용자의 membership 관한 정보 추가

--- src/main/java/a4/streamx_be/user/domain/dto/response/UserProfileResponse.java
+++ src/main/java/a4/streamx_be/user/domain/dto/response/UserProfileResponse.java
@@ -5,9 +5,10 @@ import a4.streamx_be.user.domain.entity.User;
 public record UserProfileResponse(
         String displayName,
         String email,
-        String photoUrl
+        String photoUrl,
+        MemberShipResponse membership
 ) {
     public UserProfileResponse(User user) {
-        this(user.getDisplayName(), user.getEmail(), user.getPhotoUrl());
+        this(user.getDisplayName(), user.getEmail(), user.getPhotoUrl(), new MemberShipResponse(user.getMemberShip()));
     }
 }
